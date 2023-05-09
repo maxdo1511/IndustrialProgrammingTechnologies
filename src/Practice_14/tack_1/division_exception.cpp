db72e432797f5b88a7f41e8ceaca5de06d2480a4
HBB <maxdo1579@mail.ru>
2023-05-09 15:15:38 +0300

    add practice 14

---
+++ src/Practice_14/tack_1/division_exception.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+int divison(int a, int b)
+{
+	if (b == 0)
+	{
+		throw runtime_error("Math error: cannot be divided by zero");
+	}
+	return a / b;
+}
+
+int main()
+{
+	int vareable;
+	int div;
+	cin >> vareable;
+	cin >> div;
+	try {
+		int res = divison(vareable, div);
+		cout << vareable << " / " << div << " = " << res;
+	}
+	catch (runtime_error& e)
+	{
+		cout << "Exception! " << e.what();
+	}
+}
---
+++ src/Practice_14/tack_2/index_exception.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <exception>
+
+using namespace std;
+int getItem(int mas[], int index)
+{
+	if (index < 0)
+	{
+		throw runtime_error("Index error: cannot be < 0");
+	}
+	int size = std::size_t(mas);
+	if (index >= size)
+	{
+		throw runtime_error("Index error: cannot be > size");
+	}
+	return mas[index];
+}
+
+int main()
+{
+	int mas[] = { 1, 2, 3, 5, 7 };
+	int i;
+	cin >> i;
+	try {
+		int res = getItem(mas, i);
+		cout << res;
+	}
+	catch (runtime_error& e)
+	{
+		cout << "Exception! " << e.what();
+	}
+}
